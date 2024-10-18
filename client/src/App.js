baf880c3e9ee7c54d7a52cad2592b6ab5c9c7409
Tox-XStarWake <toxicsalt0001@gmail.com>
2024-10-18 10:48:52 -0400

    major update

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+// client/src/App.js
+import React, { useEffect, useState } from 'react';
+
+function App() {
+    const [stats, setStats] = useState([]);
+
+    useEffect(() => {
+        fetch('/api/stats')
+            .then((response) => response.json())
+            .then((data) => setStats(data.games));
+    }, []);
+
+    return (
+        <div>
+            <h1>Team Stats</h1>
+            <ul>
+                {stats.map((game) => (
+                    <li key={game.id}>
+                        {game.date} - {game.opponent}: {game.score} (Top Player: {game.topPlayer})
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
